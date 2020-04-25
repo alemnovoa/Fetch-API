f0136b3da31e1c123674fbcc0d572f2e776e73ac
Alejandro Martin Novoa <alejandromnovoa@gmail.com>
2020-04-25 14:22:53 -0300

    Version FULL

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
+document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
+document.getElementById('apiBTN').addEventListener('click', cargarREST);
+
 
 
 
@@ -16,4 +19,47 @@ function cargarTXT() {
           .catch(function(error) {
                console.log(error);
           });
+}
+
+function cargarJSON(){
+     fetch('empleados.json')
+          .then(function(res) {
+               // conexion y definir
+               return res.json();
+          })
+          // muestra/respuesta
+          .then(function(data){
+               let html = '';
+               data.forEach(function(empleado){
+                    html += `
+                         <li>${empleado.nombre} ${empleado.puesto}</li>
+                    `;
+               })
+
+               document.getElementById('resultado').innerHTML = html;
+          })
+          .catch(function(error) {
+               console.log(error);
+          });
+}
+// DATOS DESDE UNA REST API
+function cargarREST (){
+     fetch('https://picsum.photos/list')
+          .then(function(res) {
+               return res.json();
+          })
+          .then (function(imagenes){
+               let html = '';
+               
+               imagenes.forEach(function(imagen) {
+                    html += `
+                         <li>
+                              <a href="${imagen.post_url}"> Ver imagen</a>
+                              ${imagen.author}
+                         </li>
+                    `;
+               });
+               
+               document.getElementById('resultado').innerHTML = html;
+          })
 }
